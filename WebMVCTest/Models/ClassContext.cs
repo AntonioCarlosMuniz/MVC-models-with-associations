4948b3cb6137e2be4ee9c7a6bf4a556ccd08c959
anton <anton@AntonioCarlos>
2018-04-04 21:49:24 -0300

    Adicionando controladores e views

---
+++ WebMVCTest/Models/ClassContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCTest.Models
+{
+    public class ClassContext : DbContext
+    {
+        public ClassContext() : base()
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+        public DbSet<Professor> Professores { get; set; }
+        public DbSet<Turma> Turmas { get; set; }
+    }
+}
---
+++ WebMVCTest/Models/Professor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVCTest.Models
+{
+    public class Professor
+    {
+        public int ProfessorId { get; set; }
+        public string Nome { get; set; }
+
+        public virtual ICollection<Disciplina> Disciplinas { get; set; }
+    }
+}
